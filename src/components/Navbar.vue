3a1cecb91ae9bcbbb7625a6de521c0389f4c25f1
Koji Eguchi <koji.eguchi@hotmail.com>
2023-06-01 16:54:32 -0400

    Install Tailwind and style Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,19 @@
 <template>
-  <header>
-     
-      <div>
-        <router-link :to="{name: 'home'}">Home</router-link>
-        <router-link :to="{name: 'favourite'}">Favourite</router-link>
-        
-        <div v-if="!isLoggedIn">
-          <router-link :to="{name: 'sign-up'}">Sign Up</router-link>
-          <router-link :to="{name: 'log-in'}">Log In</router-link>
+      <div class="bg-pink-700 flex gap-3 h-auto mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 items-center justify-around">
+        <router-link :to="{name: 'home'}" class="p-3 hover:bg-pink-800">Home</router-link>
+        <router-link :to="{name: 'favourite'}" class="p-3 hover:bg-pink-800">Favourite</router-link>
+        <div v-if="!isLoggedIn" class="flex gap-3 items-center justify-around ">
+          <router-link :to="{name: 'sign-up'}" class="p-3 hover:bg-pink-800">Sign Up</router-link>
+          
+        </div>
+        <div v-if="!isLoggedIn" class="flex gap-3 items-center justify-around ">
+          
+          <router-link :to="{name: 'log-in'}" class="p-3 hover:bg-pink-800">Log In</router-link>
         </div>
         <div v-else>
-          <button @click="logoutUser" class="logout-button" >Logout</button>
+          <button @click="logoutUser" class="p-3 hover:bg-pink-800">Logout</button>
         </div>
-        
-       
       </div>
-    </header>
 </template>
 
 <script>
